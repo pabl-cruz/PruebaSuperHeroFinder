5cff565ffea60ea0681e04f68060e99fe530f2b2
pcruz <pablo.cruz2497@gmail.com>
2024-08-01 11:46:47 -0400

    (wip) realizada conexión a superhero api

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -22,12 +22,33 @@ $(document).ready(function(){
   $('#superheroConsulta').on('click', function(event){
     event.preventDefault();
     limpiar();
+
     let idConsultado = $('#idSuperheroe').val();
-    
     let filtro = validar(idConsultado);
+
     if (filtro == true){
-      console.log(idConsultado)
-      $('#info').append(`<p>${idConsultado}</p>`);
+
+      $.ajax({
+        async: false,
+        url: `https://superheroapi.com/api.php/3a4f4e16e4ad52d85f9e4e91bbc17001/${idConsultado}`,
+        type: 'GET',
+        dataType: 'json',
+        success: function(data){
+          console.log(data.name);
+          console.log(data.powerstats);
+          console.log(data.biography);
+        },
+        error: function(){
+          alert('No se pudieron encontrar los datos');
+        },
+      })
+
+      if(idConsultado > 732){
+        alert('El código de superheroe no está en el indice. Por favor intentar de nuevo');
+        limpiar();
+      } else {
+        $('#info').append(`<p>${idConsultado}</p>`);
+      }
     }
   });
 })
