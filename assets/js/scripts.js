4503aa947d5f8110fafab319a393622cf9141f69
pcruz <pablo.cruz2497@gmail.com>
2024-08-01 17:11:31 -0400

    (wip) arreglado grafico de poder y actualizada tarjeta

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,32 +1,19 @@
 $(document).ready(function(){
                           
   function limpiar(){
-    $('#info').empty();
+    $('#tarjeta').empty();
+    $('#info h2').empty();
   }
-
-  function validar(num){
-    const filtroNum = /[1-9][0-9]*/;
-      if (
-        filtroNum.test(num) == false
-      ) {
-        alert('Por favor, introduzca un valor númerico.');
-        let esValido = false;
-        return esValido;
-      } else {
-        let esValido = true;
-        return esValido;
-      }
-    }
-
 
   $('#superheroConsulta').on('click', function(event){
     event.preventDefault();
-    //limpiar();
+    limpiar();
 
+
+    const filtroNum = /[1-9][0-9]*/;
     let idConsultado = $('#idSuperheroe').val();
-    let filtro = validar(idConsultado);
 
-    if (filtro == true){
+    if (filtroNum.test(idConsultado) && idConsultado < 732 && idConsultado >= 1){
 
       $.ajax({
         async: false,
@@ -34,57 +21,67 @@ $(document).ready(function(){
         type: 'GET',
         dataType: 'json',
         success: function(data){
+        //arreglo vacio para llenar datos de poderes
+        const dataPoints = [];
+          //for...in para recorrer entre las propiedades (llaves) de un objeto
+          for(const power in data.powerstats){
+            //agregando datapoints
+            if(data.powerstats[power] !== null){
+              dataPoints.push({ y: Number(data.powerstats[power]), indexLabel: power})
 
-          const biografia = Object.entries(data.biography);
-          const apariencia = Object.entries(data.appearance);
-          const trabajo = Object.entries(data.work);
-          const relaciones = Object.entries(data.connections);
+            } else {
+              //se pasa el dato si es null para no romper grafico
+              continue;
+            }
+          }
+          
 
-
-          const power = data.powerstats;
-      var chart = new CanvasJS.Chart("powers", {
-        exportEnabled: true,
-        animationEnabled: true,
-        title:{
-          text: "Estadisticas de poder para "+data.name
-        },
-        legend:{
-          cursor: "pointer",
-        },
+          var chart = new CanvasJS.Chart("powers", {
+            exportEnabled: true,
+            animationEnabled: true,
+            title:{
+              text: "Estadisticas de poder para "+data.name
+            },
+            legend:{
+              cursor: "pointer",
+            },
         data: [{
           type: "pie",
-          showInLegend: true,
-          toolTipContent: "{name}: <strong>{y}%</strong>",
-          indexLabel: "{name} - {y}%",
-          dataPoints: [
-            { y: parseInt(power['intelligence']), name: "Inteligencia" },
-            { y: parseInt(power['strength']), name: "Fuerza" },
-            { y: parseInt(power['speed']), name: "Velocidad" },
-            { y: parseInt(power['durability']), name: "Resistencia" },
-            { y: parseInt(power['power']), name: "Energía" },
-            { y: parseInt(power['combate']), name: "Combate" },
-          ]
+          indexLabelPlacement: "inside",
+          toolTipContent: "{indexLabel}: <strong>{y}%</strong>",
+          indexLabel: "{indexLabel}",
+          dataPoints: dataPoints,
         }]
       });
       chart.render();
 
-          if(idConsultado > 732){
+          if(idConsultado > 732 || idConsultado <= 0){
             alert('El código de superheroe no está en el indice. Por favor intentar de nuevo');
             limpiar();
           } else {
-
+            limpiar();
             $('#info').prepend(`
               <h2 class='my-3 mx-auto pl-1'>Superheroe encontrado</h2>`);
 
-            $('#contenido').append(`
-                 <div class="card col-12 col-lg-6">
+            $('#tarjeta').append(`
+                 <div class="card">
                     <img class='card-img' src='${data.image.url}' alt='${data.name}'>
                     <h3 class='card-title p-3'>${data.name}</h3>
                       <div class='card-body'>
-                        <p class='card-text'>${biografia}</p>
-                        <p class='card-text'>${apariencia}</p>
-                        <p class='card-text'>${trabajo}</p>
-                        <p class='card-text'>${relaciones}</p>
+                        <ul class="list-group list-group-flush">
+                          <li class="list-group-item"><h4>Datos Personales</h4></li>
+                          <li class="list-group-item"><strong>Nombre:</strong> ${data.biography['full-name']}</li>
+                          <li class="list-group-item"><strong>Primera Aparición:</strong> ${data.biography['first-appearance']}</li>
+                          <li class="list-group-item"><strong>Lugar de nacimiento:</strong> ${data.biography['place-of-birth']}</li>
+                          <li class="list-group-item"><strong>Alineación:</strong> ${data.biography['alignment']}</li>
+                          <li class="list-group-item"><h4>Apariencia</h4></li>
+                          <li class="list-group-item"><strong>Genero:</strong> ${data.appearance['gender']}</li>
+                          <li class="list-group-item"><strong>Raza:</strong> ${data.appearance['race']}</li>
+                          <li class="list-group-item"><strong>Altura:</strong> ${data.appearance['height'][1]}</li>
+                          <li class="list-group-item"><strong>Peso:</strong> ${data.appearance['weight'][1]}</li>
+                          <li class="list-group-item"><strong>Color de ojos:</strong> ${data.appearance['eye-color']}</li>
+                          <li class="list-group-item"><strong>Color de cabello:</strong> ${data.appearance['hair-color']}</li>
+                        </ul>
                     </div>
                   </div>
               `
@@ -95,6 +92,8 @@ $(document).ready(function(){
           alert('No se pudieron encontrar los datos');
         },
       })
+    } else {
+      alert('Por favor, introduzca un valor númerico o valido.');
     }
   });
 })
