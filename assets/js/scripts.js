33c6ad86ea6e456c0fc17fe69b1ed14eeecc44db
pcruz <pablo.cruz2497@gmail.com>
2024-08-02 12:32:16 -0400

    (wip) agregados filtros en grafico de poder y carta de información

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,99 +1,134 @@
-$(document).ready(function(){
-                          
-  function limpiar(){
-    $('#tarjeta').empty();
-    $('#info h2').empty();
+$(document).ready(function () {
+  function limpiar() {
+    $("#tarjeta").empty();
+    $("#info h2").empty();
   }
 
-  $('#superheroConsulta').on('click', function(event){
+  $("#superheroConsulta").on("click", function (event) {
     event.preventDefault();
     limpiar();
 
-
     const filtroNum = /[1-9][0-9]*/;
-    let idConsultado = $('#idSuperheroe').val();
+    let idConsultado = $("#idSuperheroe").val();
 
-    if (filtroNum.test(idConsultado) && idConsultado < 732 && idConsultado >= 1){
-
+    if (
+      filtroNum.test(idConsultado) &&
+      idConsultado < 732 &&
+      idConsultado >= 1
+    ) {
       $.ajax({
         async: false,
         url: `https://superheroapi.com/api.php/3a4f4e16e4ad52d85f9e4e91bbc17001/${idConsultado}`,
-        type: 'GET',
-        dataType: 'json',
-        success: function(data){
-        //arreglo vacio para llenar datos de poderes
-        const dataPoints = [];
+        type: "GET",
+        dataType: "json",
+        success: function (data) {
+          //arreglo vacio para llenar datos de poderes
+          const dataPoints = [];
           //for...in para recorrer entre las propiedades (llaves) de un objeto
-          for(const power in data.powerstats){
+          for (const power in data.powerstats) {
             //agregando datapoints
-            if(data.powerstats[power] !== null){
-              dataPoints.push({ y: Number(data.powerstats[power]), indexLabel: power})
-
+            if (data.powerstats[power] !== null) {
+              dataPoints.push({
+                y: Number(data.powerstats[power]),
+                indexLabel: power,
+              });
             } else {
               //se pasa el dato si es null para no romper grafico
               continue;
             }
           }
-          
 
-          var chart = new CanvasJS.Chart("powers", {
-            exportEnabled: true,
-            animationEnabled: true,
-            title:{
-              text: "Estadisticas de poder para "+data.name
-            },
-            legend:{
-              cursor: "pointer",
-            },
-        data: [{
-          type: "pie",
-          indexLabelPlacement: "inside",
-          toolTipContent: "{indexLabel}: <strong>{y}%</strong>",
-          indexLabel: "{indexLabel}",
-          dataPoints: dataPoints,
-        }]
-      });
-      chart.render();
+          const nanCount = dataPoints.filter((val) => isNaN(val.y)).length;
 
-          if(idConsultado > 732 || idConsultado <= 0){
-            alert('El código de superheroe no está en el indice. Por favor intentar de nuevo');
+          if (nanCount >= 2) {
+            $(".canvasjs-chart-container").hide();
+            $("#powers").append(
+              '<p class="no-data"><strong>No hay datos de poder para este superheroe.</strong></p>'
+            );
+          } else {
+            var chart = new CanvasJS.Chart("powers", {
+              exportEnabled: true,
+              animationEnabled: true,
+              title: {
+                text: "Estadisticas de poder para " + data.name,
+              },
+              legend: {
+                cursor: "pointer",
+              },
+              data: [
+                {
+                  type: "pie",
+                  indexLabelPlacement: "inside",
+                  toolTipContent: "{indexLabel}: <strong>{y}%</strong>",
+                  indexLabel: "{indexLabel}",
+                  dataPoints: dataPoints,
+                },
+              ],
+            });
+            chart.render();
+          }
+
+          if (idConsultado > 732 || idConsultado <= 0) {
+            alert(
+              "El código de superheroe no está en el indice. Por favor intentar de nuevo"
+            );
             limpiar();
           } else {
             limpiar();
-            $('#info').prepend(`
+            $("#info").prepend(`
               <h2 class='my-3 mx-auto pl-1'>Superheroe encontrado</h2>`);
 
-            $('#tarjeta').append(`
-                 <div class="card">
+            $("#tarjeta").append(`
+                 <div class="card mb-5">
                     <img class='card-img' src='${data.image.url}' alt='${data.name}'>
                     <h3 class='card-title p-3'>${data.name}</h3>
                       <div class='card-body'>
                         <ul class="list-group list-group-flush">
-                          <li class="list-group-item"><h4>Datos Personales</h4></li>
-                          <li class="list-group-item"><strong>Nombre:</strong> ${data.biography['full-name']}</li>
-                          <li class="list-group-item"><strong>Primera Aparición:</strong> ${data.biography['first-appearance']}</li>
-                          <li class="list-group-item"><strong>Lugar de nacimiento:</strong> ${data.biography['place-of-birth']}</li>
-                          <li class="list-group-item"><strong>Alineación:</strong> ${data.biography['alignment']}</li>
-                          <li class="list-group-item"><h4>Apariencia</h4></li>
-                          <li class="list-group-item"><strong>Genero:</strong> ${data.appearance['gender']}</li>
-                          <li class="list-group-item"><strong>Raza:</strong> ${data.appearance['race']}</li>
-                          <li class="list-group-item"><strong>Altura:</strong> ${data.appearance['height'][1]}</li>
-                          <li class="list-group-item"><strong>Peso:</strong> ${data.appearance['weight'][1]}</li>
-                          <li class="list-group-item"><strong>Color de ojos:</strong> ${data.appearance['eye-color']}</li>
-                          <li class="list-group-item"><strong>Color de cabello:</strong> ${data.appearance['hair-color']}</li>
+                          <li class="title-list"><h4>Datos Personales</h4></li>
+                          <li class="list-group-item"><strong>Nombre:</strong> <span>${data.biography["full-name"]}</span></li>
+                          <li class="list-group-item"><strong>Primera Aparición:</strong> <span>${data.biography["first-appearance"]}</span></li>
+                          <li class="list-group-item"><strong>Lugar de nacimiento:</strong> <span>${data.biography["place-of-birth"]}</span></li>
+                          <li class="list-group-item"><strong>Alineación:</strong> <span>${data.biography["alignment"]}</span></li>
+                          <li class="title-list"><h4>Apariencia</h4></li>
+                          <li class="list-group-item"><strong>Genero:</strong> <span>${data.appearance["gender"]}</span></li>
+                          <li class="list-group-item"><strong>Raza:</strong> <span>${data.appearance["race"]}</span></li>
+                          <li class="list-group-item"><strong>Altura:</strong> <span>${data.appearance["height"][1]}</span></li>
+                          <li class="list-group-item"><strong>Peso:</strong> <span>${data.appearance["weight"][1]}</span></li>
+                          <li class="list-group-item"><strong>Color de ojos:</strong> <span>${data.appearance["eye-color"]}</span></li>
+                          <li class="list-group-item"><strong>Color de cabello:</strong> <span>${data.appearance["hair-color"]}</span></li>
+                          <li class="title-list"><h4>Ocupaciones</h4></li>
+                          <li class="list-group-item"><strong>Trabajo(s):</strong> <span>${data.work["occupation"]}</span></li>
+                          <li class="list-group-item"><strong>Base:</strong> <span>${data.work["base"]}</span></li>
+                          <li class="title-list"><h4>Relaciones</h4></li>
+                          <li class="list-group-item"><strong>Afiliación(es):</strong> <span>${data.connections["group-affiliation"]}</span></li>
+                          <li class="list-group-item"><strong>Parientes:</strong> <span>${data.connections["relatives"]}</span></li>
                         </ul>
                     </div>
                   </div>
-              `
-            )
+              `);
+              
+            const infoElemento = $('.list-group-item span')
+            infoElemento.each(function(){
+              if($(this).text().trim() === '-' || $(this).text() === "" || $(this).text() === "null"){
+                $(this).remove();
+              }
+            })
+            const lista = $('.list-group-item')
+            const listaTitulo = $('.list-group-item h4')
+            lista.each(function(){
+              const haySpan = $(this).find('span');
+              if(haySpan.length === 0){
+                $(this).remove();
+              }
+            })
           }
         },
-        error: function(){
-          alert('No se pudieron encontrar los datos');
+        error: function () {
+          alert("No se pudieron encontrar los datos");
         },
-      })
+      });
     } else {
-      alert('Por favor, introduzca un valor númerico o valido.');
+      alert("Por favor, introduzca un valor númerico o valido.");
     }
   });
-})
+});
